911291fd91e2a8593eecd3da43b6b7e7da1f77dd
Jon Bolt <epic.bagel@gmail.com>
2013-12-11 22:47:38 +0000

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+import django_test_views
+setup(name='django-heroku-sendgridify',
+	version=sendgridify.__version__,
+	packages = find_packages(),
+	license='The MIT License',
+	platforms=['OS Independent'],
+	keywords='django, send grid',
+	author='Jon Bolt',
+	author_email='jon@epicbagel.com',
+	url="https://github.com/epicbagel/django-heroku-sendgridify",
+)
